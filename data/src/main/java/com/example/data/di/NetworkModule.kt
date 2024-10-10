fea14b63236777e84abbb9d8270d4ae9763244aa
Drakee <thanhlong9123@gmail.com>
2024-10-10 22:58:29 +0700

    edit: change product source

--- data/src/main/java/com/example/data/di/NetworkModule.kt
+++ data/src/main/java/com/example/data/di/NetworkModule.kt
@@ -1,11 +1,13 @@
 package com.example.data.di
 
+import android.util.Log
 import com.example.data.network.NetworkServiceImplement
 import com.example.domain.network.NetworkService
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
@@ -24,6 +26,11 @@ val networkModule = module {
             // Log everything
             install(Logging) {
                 level = LogLevel.ALL
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Log.d("Backend", message)
+                    }
+                }
             }
         }
     }
--- data/src/main/java/com/example/data/repository/CategoryRepositoryImplement.kt
+++ data/src/main/java/com/example/data/repository/CategoryRepositoryImplement.kt
@@ -1,11 +1,12 @@
 package com.example.data.repository
 
+import com.example.domain.model.CategoriesListModel
 import com.example.domain.network.NetworkService
 import com.example.domain.network.ResultWrapper
 import com.example.domain.repository.CategoryRepository
 
 class CategoryRepositoryImplement(private val networkService: NetworkService): CategoryRepository {
-    override suspend fun getCategories(): ResultWrapper<List<String>> {
+    override suspend fun getCategories(): ResultWrapper<CategoriesListModel> {
         return networkService.getCategories()
     }
 }
