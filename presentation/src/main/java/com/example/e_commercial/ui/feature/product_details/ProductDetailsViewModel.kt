06f82066005e29ba8797371647f3f043dddaaeba
Drakee <thanhlong9123@gmail.com>
2024-10-14 18:25:53 +0700

    feat: implement add product to cart feature & cart UI

--- presentation/src/main/java/com/example/e_commercial/ui/feature/product_details/ProductDetailsViewModel.kt
+++ presentation/src/main/java/com/example/e_commercial/ui/feature/product_details/ProductDetailsViewModel.kt
@@ -1,6 +1,50 @@
 package com.example.e_commercial.ui.feature.product_details
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.model.request.AddCartRequestModel
+import com.example.domain.usecase.AddToCartUseCase
+import com.example.e_commercial.model.UIProductModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class ProductDetailsViewModel: ViewModel() {
+class ProductDetailsViewModel(val useCase: AddToCartUseCase) : ViewModel() {
+
+    private val _state = MutableStateFlow<ProductDetailsEvent>(ProductDetailsEvent.Nothing)
+    val state = _state.asStateFlow()
+
+    fun addProductToCart(product: UIProductModel) {
+        viewModelScope.launch {
+            _state.value = ProductDetailsEvent.Loading
+            val result = useCase.execute(
+                AddCartRequestModel(
+                    product.id,
+                    product.title,
+                    product.price,
+                    1,
+                    1
+                )
+            )
+            Log.d("ProductDetailsViewModel", "Result: $result")
+            when (result) {
+                is com.example.domain.network.ResultWrapper.Success -> {
+                    _state.value = ProductDetailsEvent.Success("Product added to cart")
+                }
+
+                is com.example.domain.network.ResultWrapper.Failure -> {
+                    _state.value = ProductDetailsEvent.Error("Something went wrong! product detail")
+                }
+            }
+        }
+    }
+}
+
+
+sealed class ProductDetailsEvent {
+    data object Loading : ProductDetailsEvent()
+    data object Nothing : ProductDetailsEvent()
+    data class Success(val message: String) : ProductDetailsEvent()
+    data class Error(val message: String) : ProductDetailsEvent()
 }
